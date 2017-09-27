fb23870e83c71a3a21b9be7ff14a6dac57e066a0
chenxingfeng <chenxingfeng@huarongcaifu.com.cn>
2017-09-27 17:31:32 +0800

    new project

---
+++ src/main/java/com/cxf/boot/Main.java
@@ -0,0 +1,60 @@
+/**
+ * Project Name:kyle
+ * File Name:Main.java
+ * Package Name:com.cxf.boot
+ * Date:2017年9月27日下午2:32:39
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ *
+ */
+
+package com.cxf.boot;
+
+import com.cxf.logger.Logs;
+
+/**
+ * Description: <br/>
+ * Date: 2017年9月27日 下午2:32:39 <br/>
+ * 
+ * @author cxf
+ * @version
+ * @see
+ */
+public class Main {
+	/**
+	 * 源码启动请不要直接运行此方法，否则不能正确加载配置文件
+	 * 
+	 * @param args
+	 *            启动参数
+	 */
+	public static void main(String[] args) {
+		Logs.init();
+		Logs.Console.info("launch mpush server...");
+		final ServerLauncher launcher = new ServerLauncher();
+		launcher.init();
+		launcher.start();
+		addHook(launcher);
+	}
+
+	/**
+	 * 注意点 1.不要ShutdownHook Thread 里调用System.exit()方法，否则会造成死循环。
+	 * 2.如果有非守护线程，只有所有的非守护线程都结束了才会执行hook 3.Thread默认都是非守护线程，创建的时候要注意
+	 * 4.注意线程抛出的异常，如果没有被捕获都会跑到Thread.dispatchUncaughtException
+	 * 
+	 * @param launcher
+	 */
+	private static void addHook(final ServerLauncher launcher) {
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() { // 一般做法
+					public void run() {
+						try {
+							launcher.stop();
+						} catch (Exception e) {
+							Logs.Console.error("mpush server stop ex", e);
+						}
+						Logs.Console.info("jvm exit, all service stopped.");
+					}
+				})
+
+		);
+	}
+
+}
---
+++ src/main/java/com/cxf/logger/Logs.java
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *   ohun@live.cn (夜色)
+ */
+
+package com.cxf.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by ohun on 2016/5/16.
+ * 
+ * @author ohun@live.cn
+ */
+public class Logs {
+
+	public static void init() {
+		System.setProperty("logback.configurationFile", "log4j.properties");
+	}
+
+	public final static Logger Console = LoggerFactory.getLogger("console"),
+
+	CONN = LoggerFactory.getLogger("cxf.conn.log"),
+
+	PUSH = LoggerFactory.getLogger("cxf.push.log"),
+
+	HTTP = LoggerFactory.getLogger("cxf.http.log"),
+
+	TCP = LoggerFactory.getLogger("cxf.tcp.log");
+
+}
